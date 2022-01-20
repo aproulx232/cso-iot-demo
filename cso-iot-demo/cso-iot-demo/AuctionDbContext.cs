e49ef5181e83df30aba2275c0bde1f1af58e0f96
Allen Proulx <Allen.Proulx@adesa.com>
2022-01-19 23:46:13 -0500

    Add AuctionDbContext

---
+++ cso-iot-demo/cso-iot-demo/AuctionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cso_iot_demo
+{
+    public class AuctionDbContext : DbContext
+    {
+
+        public AuctionDbContext(DbContextOptions options) : base(options) { }
+
+        
+    }
+}
--- cso-iot-demo/cso-iot-demo/DeviceConsumerFunction.cs
+++ cso-iot-demo/cso-iot-demo/DeviceConsumerFunction.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -5,10 +6,20 @@ namespace cso_iot_demo
 {
     public class DeviceConsumerFunction
     {
+        private readonly IRepairItemRepository _repairItemRepository;
+
+        public DeviceConsumerFunction(IRepairItemRepository repairItemRepository)
+        {
+            _repairItemRepository = repairItemRepository;
+        }
+
         [FunctionName(nameof(DeviceConsumerFunction))]
-        public void Run([ServiceBusTrigger("%DeviceQueueName%", Connection = "ConnectionStrings:DeviceServiceBus")]string deviceMessage, ILogger log)
+        public async Task Run([ServiceBusTrigger("%DeviceQueueName%", Connection = "ConnectionStrings:DeviceServiceBus")]string deviceMessage, ILogger log)
         {
             log.LogInformation($"Processing Device Message: {deviceMessage}");
+            deviceMessage = "[\"202741038: 1: PSI 7 Day: \"]";
+
+            await _repairItemRepository.CompleteRepairItem(202741039, 1, 19, 13);
         }
     }
 }
